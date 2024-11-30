9c3bd9e0335a14413979df2d20cc71ebcc60a77e
Asminstha <asmin.stha77@gmail.com>
2024-11-30 11:07:56 +0545

    Navbar Design Update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,6 +20,8 @@ const Navbar = () => {
         <div className="nav-login-cart">
             <button>Login</button>
             <img src={cart_icon} alt="" />
+            {/* counter for cart  */}
+            <div className="nav-cart-count">0</div>
         </div>
     </div>
   )
