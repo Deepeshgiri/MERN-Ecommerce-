25e828f149923615680b388d2bafaf18856aa0ff
Asmin Shrestha <asmin.stha77@gmail.com>
2025-01-21 19:17:42 +0545

    login page created 0

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 export const Product = () => {
+  // const{all_product}=useContext(ShopContext);
   return (
-    <div>Product</div>
+    
+
+  <h1>Now creating product page</h1>
   )
 }
